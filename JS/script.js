57ab6d9ce5ddfde48452708ed76327862cb6539d
NathanAtaliba <100451579+NathanAtaliba@users.noreply.github.com>
2023-01-12 21:32:07 -0300

    Meu_Portifolio

---
+++ JS/script.js
@@ -0,0 +1,27 @@
+
+function home(){
+    var oculta = document.getElementById("oculta");
+    var name = document.getElementById("name");
+    console.log("home");
+    oculta.style.background = '#9370DB';
+    name.style.color = '#9370DB' ;
+
+}
+function projects(){
+    console.log("projects");
+    var name = document.getElementById("name");
+    oculta.style.background = '#FFFFFF';
+    name.style.color = '#FFFFFF' ;
+}
+function summary(){
+    console.log("summary");
+    var name = document.getElementById("name");
+    oculta.style.background = 'Crimson';
+    name.style.color = 'Crimson' ;
+}
+function contacts(){
+    console.log("contacts");
+    var name = document.getElementById("name");
+    oculta.style.background = 'Khaki';
+    name.style.color = 'Khaki'
+}
